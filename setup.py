5be7573640b35491f8ac2f08eb8795ae1ccbed0d
Nico Weil <nico.weil@web.de>
2022-02-23 17:19:06 +0100

    Set up Python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""This file enables installing this package with 'pip install -e'.
+
+Configuration is done in 'setup.cfg'.
+"""
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
---
+++ src/backupbot/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""A dummy main module."""
+
+def main() -> None:
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
