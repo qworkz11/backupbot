b48b722094be44cebe0a25e92caca242be9257f5
Nico Weil <nico.weil@web.de>
2022-04-15 16:50:44 +0200

    Removed unnecessary tests

--- tests/unit/docker/test_docker_backup.py
+++ tests/unit/docker/test_docker_backup.py
@@ -108,25 +108,3 @@ def test_docker_backup__make_backup_name_creates_correct_name() -> None:
 
     assert dba._make_backup_name(Path("/path/to/data"), "data_container") == "data_container-data"
     assert dba._make_backup_name(Path("directory"), "data_container") == "data_container-directory"
-
-
-def test_docker_backup_backup_host_directory_backs_up_data_in_extisting_target_directory(tmp_path: Path) -> None:
-    dba = DockerBackupAdapter()
-
-    tmp_path.joinpath("data").mkdir()
-    tmp_path.joinpath("backup", "container", "bind_mounts").mkdir(parents=True)
-
-    dba.backup_host_directory(tmp_path.joinpath("data"), tmp_path.joinpath("backup"), "container")
-
-    assert tmp_path.joinpath("backup", "container", "bind_mounts", "container-data.tar.gz").is_file()
-
-
-def test_docker_backup_backup_host_directory_creates_folder_structure_if_not_exitsing(tmp_path: Path) -> None:
-    dba = DockerBackupAdapter()
-
-    tmp_path.joinpath("data").mkdir()
-    tmp_path.joinpath("backup").mkdir(parents=True)
-
-    dba.backup_host_directory(tmp_path.joinpath("data"), tmp_path.joinpath("backup"), "container")
-
-    assert tmp_path.joinpath("backup", "container", "bind_mounts", "container-data.tar.gz").is_file()
