1a8e20c94c92884eba9a64ea1d43e3fa7a55f22d
Nico Weil <nico.weil@web.de>
2022-04-19 18:57:26 +0200

    Added docker-compose utils

---
+++ src/backupbot/docker/container_utils.py
@@ -0,0 +1,71 @@
+from calendar import c
+from contextlib import contextmanager
+from pathlib import Path
+from subprocess import CompletedProcess, run
+
+from docker import DockerClient
+from docker.models.containers import Container
+
+
+@contextmanager
+def stop_and_restart_container(client: DockerClient, container_name: str, timeout: int = 20) -> None:
+    container_status = client.containers.get(container_name).status
+
+    if container_status == "running":
+        client.containers.get(container_name).stop(timeout=timeout)
+        yield None
+        client.containers.get(container_name).restart(timeout=timeout)
+    else:
+        raise RuntimeError(
+            f"Container '{container_name}' must be runnung to be stopped and restarted, but is: '{container_status}'."
+        )
+
+
+def docker_compose_up(compose_file: Path) -> None:
+    if not compose_file.is_file() or not compose_file.name.lower().endswith(".yaml"):
+        raise RuntimeError(f"Failed to call docker-compose up: Compose file must be of type .yaml: '{compose_file}'.")
+
+    args = ("docker-compose", "-f", compose_file.absolute(), "up", "-d")
+
+    result: CompletedProcess = run(args)
+
+    if result.returncode != 0:
+        raise RuntimeError(f"Failed to call docker-compose up: '{result.stdout}'.")
+
+
+def docker_compose_start(compose_file: Path) -> None:
+    if not compose_file.is_file() or not compose_file.name.lower().endswith(".yaml"):
+        raise RuntimeError(
+            f"Failed to call docker-compose restart: Compose file must be of type .yaml: '{compose_file}'."
+        )
+
+    args = ("docker-compose", "-f", compose_file.absolute(), "start")
+
+    result: CompletedProcess = run(args)
+
+    if result.returncode != 0:
+        raise RuntimeError(f"Failed to call docker-compose start: '{result.stderr}'.")
+
+
+def docker_compose_stop(compose_file: Path) -> None:
+    if not compose_file.is_file() or not compose_file.name.lower().endswith(".yaml"):
+        raise RuntimeError(f"Failed to call docker-compose stop: Compose file must be of type .yaml: '{compose_file}'.")
+
+    args = ("docker-compose", "-f", compose_file.absolute(), "stop")
+
+    result: CompletedProcess = run(args)
+
+    if result.returncode != 0:
+        raise RuntimeError(f"Failed to call docker-compose stop: '{result.stderr}'.")
+
+
+def docker_compose_down(compose_file: Path) -> None:
+    if not compose_file.is_file() or not compose_file.name.lower().endswith(".yaml"):
+        raise RuntimeError(f"Failed to call docker-compose down: Compose file must be of type .yaml: '{compose_file}'.")
+
+    args = ("docker-compose", "-f", compose_file.absolute(), "down")
+
+    result: CompletedProcess = run(args)
+
+    if result.returncode != 0:
+        raise RuntimeError(f"Failed to call docker-compose down: '{result.stderr}'.")
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,13 @@
 
 """Testing fixtures."""
 
+from contextlib import contextmanager
+from email.generator import Generator
 from pathlib import Path
 
 import pytest
+from docker import DockerClient, from_env
+from docker.errors import NotFound
 
 
 @pytest.fixture
@@ -35,3 +39,47 @@ def dummy_bind_mount_dir() -> Path:
         Path: _description_
     """
     return Path(__file__).parent.joinpath("resources", "sample_bind_mount_dir")
+
+
+@pytest.fixture(scope="session")
+def docker_client() -> DockerClient:
+    """Returns the host's docker client.
+
+    Returns:
+        DockerClient: Docker client instance.
+    """
+    client = from_env()
+    return client
+
+
+@pytest.fixture
+def sample_docker_compose_project_dir() -> Path:
+    return Path(__file__).parent.joinpath("resources", "sample_docker_compose_service")
+
+
+@pytest.fixture(scope="function")
+@contextmanager
+def running_sleeping_ubuntu() -> str:
+    client = from_env()
+
+    container_name = "sleeping_container"
+
+    try:
+        client.containers.get(container_name)
+        if client.containers.get(container_name).status == "exited":
+            # else container is running
+            client.containers.get(container_name).restart()
+    except NotFound:
+        # if the container name does not exist start a new container
+        client.containers.run("ubuntu:latest", "sleep_infinity", name=container_name, detach=True)
+
+    # if client.containers.get(container_name).status == "running":
+    yield container_name
+
+    try:
+        if client.containers.get(container_name).status == "running":
+            client.containers.get(container_name).stop()
+        client.containers.get(container_name).remove()
+    except:
+        # container does not exist anymore
+        pass
